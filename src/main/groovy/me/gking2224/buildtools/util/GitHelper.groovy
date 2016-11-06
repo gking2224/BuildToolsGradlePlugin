2b9fbe6c22b76a8764da301dc5895f1a7fe89c28
Graham King <gking2224@gmail.com>
2016-11-06 14:18:08 +0000

    remove debug stmts

--- src/main/groovy/me/gking2224/buildtools/util/GitHelper.groovy
+++ src/main/groovy/me/gking2224/buildtools/util/GitHelper.groovy
@@ -26,7 +26,6 @@ class GitHelper {
     def static GitHelper instance
 
     def GitHelper(def username, def password) {
-        println "Creating git credentials provider with username $username and password ...${password.substring(password.length()-3)}"
         credentialsProvider = new UsernamePasswordCredentialsProvider(username, password)
         sshSessionFactory = new JschConfigSessionFactory() {
                     @Override
@@ -74,7 +73,6 @@ class GitHelper {
     }
 
     def commitFile(File f, String pattern, String message) {
-        println "Committing $pattern from repository ${f.absolutePath}"
         Git git = Git.open(f)
         DirCache dc = git.add().addFilepattern(pattern).setUpdate(true).call()
         dc.getEntriesWithin(pattern).each{DirCacheEntry e-> println e.getPathString()}
